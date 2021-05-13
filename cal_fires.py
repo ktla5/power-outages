707272b593f1fc448ffb57c12d0f4bfaf8183a0c
ktla5 <78561356+ktla5@users.noreply.github.com>
2021-05-13 15:43:23 -0700

    add files via upload

---
+++ cal_fires.py
@@ -0,0 +1,66 @@
+from outages_scrapers import Scraper
+from bs4 import BeautifulSoup as Soup
+import requests
+
+
+class FireScraper(Scraper):
+    owner = "ktla5"
+    repo = "cal-fires-history"
+
+    def __init__(self, token, incident_id, name):
+        self.incident_id = incident_id
+        self.filepath = "cal-fires/{}-{}.json".format(
+            self.incident_id, name.lower().strip().replace(" ", "-")
+        )
+        super().__init__(token)
+
+    def fetch_data(self):
+        soup = Soup(
+            requests.get(
+                "http://www.fire.ca.gov/current_incidents/incidentdetails/Index/{}".format(
+                    self.incident_id
+                )
+            ).content,
+            "html5lib",
+        )
+        table = soup.select(".incident_table")[0]
+        info = {"summary": table["summary"]}
+        for tr in table.select("tr"):
+            try:
+                title = tr.select("td.emphasized")[0]
+                info[title.text.strip().rstrip(":")] = title.find_next(
+                    "td"
+                ).text.strip()
+            except:
+                pass
+        # Split out latitude and longitude, if available
+        if "Long/Lat" in info:
+            longitude, latitude = info["Long/Lat"].split("/")
+            info["latitude"] = latitude
+            info["longitude"] = longitude
+        return info
+
+
+def load_scrapers(token):
+    # Scrape the list of fires
+    url = "http://www.fire.ca.gov/current_incidents/"
+    page = 1
+    incidents = []  # List of (id, name) tuples
+    while True:
+        html = requests.get("{}?page={}".format(url, page)).text
+        soup = Soup(html, "html5lib")
+        scraped_incidents = [
+            (t.get("id"), t.get("title"))
+            for t in soup.select("table.incident_table")
+            if t.get("id")
+        ]
+        if not scraped_incidents:
+            break
+        incidents.extend(scraped_incidents)
+        page += 1
+        if "?page={}".format(page) not in html:
+            break
+        if page > 100:
+            break
+    # Return a scraper for each incident
+    return [FireScraper(token, incident_id, name) for incident_id, name in incidents]
---
+++ sce.py
@@ -0,0 +1,38 @@
+from outages_scrapers import DeltaScraper
+import requests
+
+
+class SCEOutages(DeltaScraper):
+    url = "https://prodms.dms.sce.com/outage/v1/power/outage"
+    owner = "ktla5"
+    repo = "sce-power-outages"
+    filepath = "sce-outages.json"
+
+    record_key = "incidentId"
+    noun = "incident"
+
+    def fetch_data(self):
+        data = requests.get(self.url, timeout=10).json()
+        return data["outageMapDataResponse"]["AOCIncidents"]["incident"]
+
+    def display_record(self, outage):
+        # Need to .strip() trailing whitespace from county
+        fixed = {**outage, **{"countyName": (outage.get("countyName") or "").strip()}}
+        display = []
+        display.append(
+            "  {incidentId} in {cityName} {countyName} affecting {numberOfCustomersAffected}".format(
+                **fixed
+            )
+        )
+        display.append(
+            "    https://www.google.com/maps/search/{centroidY},{centroidX}".format(
+                **fixed
+            )
+        )
+        display.append(
+            "    {memoCauseCodeDescription} - {crewStatusCodeDescription}".format(
+                **fixed
+            )
+        )
+        display.append("")
+        return "\n".join(display)
